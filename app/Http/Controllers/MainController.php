b67a5e082cf6fb06f6968101c1da09db3dd2b276
Sergey Dmitriev <dmitrieffserzh@yandex.ru>
2019-01-10 14:31:44 +0300

    update

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class MainController extends Controller {
 
     public function __construct() {
-        $this->middleware('auth');
+       // $this->middleware('auth');
     }
 
     public function index() {
