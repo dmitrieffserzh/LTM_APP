320560c7c264f9731532e55910cf0f88c5f22104
Sergey Dmitriev <dmitrieffserzh@yandex.ru>
2019-01-10 14:53:14 +0300

    update

--- database/migrations/2019_01_10_083907_create_sessions_table.php
+++ database/migrations/2019_01_10_083907_create_sessions_table.php
@@ -4,15 +4,9 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSessionsTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
+class CreateSessionsTable extends Migration {
+
+    public function up() {
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->unique();
             $table->unsignedInteger('user_id')->nullable();
@@ -23,13 +17,7 @@ class CreateSessionsTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('sessions');
     }
 }
