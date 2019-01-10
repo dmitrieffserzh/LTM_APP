4decbdc4c38ac458a1e531307de449a25173e10f
Sergey Dmitriev <dmitrieffserzh@yandex.ru>
2019-01-10 13:22:10 +0300

    Initial commit

---
+++ resources/lang/ru/auth.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'login' => 'Войти',
+    'login_email' => 'E-mail',
+    'login_password' => 'Пароль',
+    'login_remember_me' => 'Запомнить',
+    'login_forgot_password' => 'Забыли пароль?',
+
+    'register' => 'Регистрация',
+    'register_name' => 'Имя',
+    'register_email' => 'E-mail',
+    'register_password' => 'Пароль',
+    'register_password_confirmation' => 'Подтвердите пароль',
+
+    'verify' => 'Проверьте свой адрес электронной почты',
+    'verify_refresh' => 'На ваш адрес электронной почты отправлена новая ссылка для подтверждения',
+    'verify_check_your_email' => 'Прежде чем продолжить, проверьте свою электронную почту на наличие ссылки для подтверждения.',
+    'verify_not_receive' => 'Если вы не получили письмо',
+    'verify_request_repeatedly' => 'нажмите здесь, чтобы запросить повторно',
+
+    'logout' => 'Выйти',
+    'failed'   => 'Имя пользователя и пароль не совпадают.',
+    'throttle' => 'Слишком много попыток входа. Пожалуйста, попробуйте еще раз через :seconds секунд.',
+
+];
